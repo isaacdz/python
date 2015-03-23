e5e526064f955e059da5a252be98e27ee15ef0ac
isaacdz <ishaklahlouh@gmail.com>
2015-03-23 21:16:19 +0100

    Update Remmember.c

--- Remmember.c
+++ Remmember.c
@@ -6,3 +6,23 @@
            a++;
 
            }
+           
+           struct  page {
+
+   int  numpg ;
+   int nbrocc ;
+   struct  page *next ;
+ };
+
+ typedef struct page page ;
+
+struct mot1  {
+
+    page *prempg ;
+    page *derpg ;
+    char *motindex;
+    struct mot1 *next ;
+};
+
+typedef struct mot1 mot ;
+
